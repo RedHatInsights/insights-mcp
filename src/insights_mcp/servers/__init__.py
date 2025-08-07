dc6245bfd7bbc1b112343495746cffae3407c7ac
Patrik Segedy <psegedy@redhat.com>
2025-08-07 17:03:02 +0200

    chore: add example how to create server for new insights app

---
+++ src/insights_mcp/servers/__init__.py
@@ -0,0 +1,5 @@
+"""Red Hat Insights MCP servers package.
+
+This package contains specialized MCP server implementations for different
+Red Hat Insights services, such as Image Builder and Vulnerability Management.
+"""
