bac13c79d25df1e6de8ec1009c3281038cb9c7a8
Patrik Segedy <psegedy@redhat.com>
2025-08-07 14:57:34 +0200

    refactor: delete image builder client

--- src/image_builder_mcp/__init__.py
+++ src/image_builder_mcp/__init__.py
@@ -1,6 +1,5 @@
 """Image Builder MCP - A Model Context Protocol server for Red Hat Image Builder."""
 
-from .client import ImageBuilderClient
 from .server import ImageBuilderMCP
 
-__all__ = ["ImageBuilderMCP", "ImageBuilderClient"]
+__all__ = ["ImageBuilderMCP"]
